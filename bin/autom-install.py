6cd9a3954bea526be3f5b6a3640c3d975df32198
Pedestria <42527102+Pedestria@users.noreply.github.com>
2021-05-13 14:02:33 -0400

    quick fixes

--- bin/autom-install.py
+++ bin/autom-install.py
@@ -16,8 +16,8 @@ class AUTOMInstall:
             return string 
         else:
             return var_regex.sub(self.installPrefix,string)
-    def process(self):
-        j = json.load(io.open("./AUTOMINSTALL","r"))
+    def process(self,stream:io.TextIOWrapper):
+        j = json.load(stream)
         assert(j.get("entries"))
         entries:"list[dict]" = j.get('entries')
         for e in entries:
@@ -33,15 +33,24 @@ class AUTOMInstall:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--install-prefix",type=str,dest="install_prefix")
+    parser.add_argument("--config",type=str,required=False)
     args = parser.parse_args()
 
     if args.install_prefix is None:
         raise "Install Prefix Not Provided.\nExiting..."
 
-    if os.path.exists("./AUTOMINSTALL"):
-       AUTOMInstall(args.install_prefix).process()
+    autom_install_file:str
+
+    if args.config is not None:
+       autom_install_file = os.path.join(args.config,"AUTOMINSTALL")
+    else:
+        autom_install_file ="./AUTOMINSTALL"
+
+
+    if os.path.exists(autom_install_file):
+       AUTOMInstall(args.install_prefix).process(io.open(autom_install_file,"r"))
     else: 
-        raise FileNotFoundError("./AUTOMINSTALL")
+        raise FileNotFoundError(autom_install_file)
 
 if __name__ == "__main__":
     main()
