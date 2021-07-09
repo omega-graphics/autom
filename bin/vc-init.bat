82f6fdf30f838935359179bcb339a22d6153f628
Pedestria <42527102+Pedestria@users.noreply.github.com>
2021-07-08 20:18:08 -0400

    Update vc-init.bat

--- bin/vc-init.bat
+++ bin/vc-init.bat
@@ -1,5 +1,5 @@
 vswhere -nologo -latest -property installationPath > loc.txt 
 set /p loc=<loc.txt 
 del loc.txt
-"%loc%\VC\Auxiliary\Build\vcvarsall.bat" amd64
+"%loc%\VC\Auxiliary\Build\vcvarsall.bat" %PROCESSOR_ARCHITECTURE%
 
